17b6bb3656bd3b76bc9de870da66abd7c87028f2
Laxman Rumba <rumbavai767@gmail.com>
2025-07-06 23:52:35 +0545

    add get stocks endpoint

---
+++ FinTech/Controllers/StockController.cs
@@ -0,0 +1,30 @@
+﻿using FinTech.Data;
+using FinTech.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinTech.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public ActionResult<List<Stock>> GetStocks()
+        {
+            var stocks = _context.Stocks.ToList();
+            if(stocks == null || stocks.Count==0)
+            {
+                return NotFound( new { message="No stocks found.", success=false });
+            }
+            return Ok( new { message="Stocks fetched succesfully", success=true, stocks });
+        }
+
+    }
+}
