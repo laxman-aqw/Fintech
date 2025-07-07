f2eb887b431654c31233c93122fd29b544a4f767
Laxman Rumba <rumbavai767@gmail.com>
2025-07-07 08:15:00 +0545

    Add DTO in Stock

--- FinTech/Controllers/StockController.cs
+++ FinTech/Controllers/StockController.cs
@@ -1,7 +1,10 @@
 ﻿using FinTech.Data;
+using FinTech.Dtos.Stock;
+using FinTech.Mappers;
 using FinTech.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinTech.Controllers
 {
@@ -15,15 +18,24 @@ namespace FinTech.Controllers
             _context = context;
         }
 
-        [HttpGet("")]
-        public ActionResult<List<Stock>> GetStocks()
+        [HttpGet]
+        public async Task<ActionResult<List<StockDto>>> GetStocks()
         {
-            var stocks = _context.Stocks.ToList();
-            if(stocks == null || stocks.Count==0)
+            var stocks = (await _context.Stocks.ToListAsync()).Select(s=>s.ToStockDto()).ToList();
+            if(stocks.Count==0)
             {
                 return NotFound( new { message="No stocks found.", success=false });
             }
-            return Ok( new { message="Stocks fetched succesfully", success=true, stocks });
+            return Ok(stocks);
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<ActionResult<StockDto>> GetStockById(int id) {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null) { 
+                return NotFound(new { message = "Stock not found with current id.", success = false });
+            }
+            return Ok(stock.ToStockDto());
         }
 
     }
---
+++ FinTech/Mappers/StockMappers.cs
@@ -0,0 +1,21 @@
+﻿using FinTech.Dtos.Stock;
+using FinTech.Models;
+
+namespace FinTech.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel) {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap,
+            };
+        }
+    }
+}
